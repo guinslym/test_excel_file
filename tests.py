25b7c221f966ea7b797c61feebfad442e9db09d4
guinslym <guinslym@gmail.com>
2017-10-19 17:08:32 -0400

    inital commit

---
+++ tests.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+import xlsxwriter
+import xlrd
+
+class TestExcelFile(TestCase):
+
+    def test_a_new_file_is_created(self):
+        pass
+
+    def test_number_of_sheets_in_the_excel_sheets(self):
+        pass
+
+    def test_name_of_the_worksheets(self):
+        pass
+
+
+class TestSheet1(TestCase):
+    def test_header_content(self):
+        pass
+
+class TestSheet2(TestCase):
+    def test_header_content(self):
+        pass
