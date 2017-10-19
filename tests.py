1dc940ab1358c17f130aa38a80017b61a7919c91
guinslym <guinslym@gmail.com>
2017-10-19 18:06:27 -0400

    initial commit

--- tests.py
+++ tests.py
@@ -1,23 +1,46 @@
 from unittest import TestCase
 import xlsxwriter
 import xlrd
+import os
+import openpyxl 
 
 class TestExcelFile(TestCase):
 
     def test_a_new_file_is_created(self):
-        pass
+        file_exist = os.path.isfile('./distribution.xslx')
 
     def test_number_of_sheets_in_the_excel_sheets(self):
-        pass
+        workbook = xlsxwriter.WorkBook('./distribution.xslx')
+        self.assertEqual(
+            2,
+            len(workbook.sheetnames)
+            'The Number of Worksheets should be equal to 2'
+        )
 
     def test_name_of_the_worksheets(self):
-        pass
+        workbook = xlsxwriter.WorkBook('./distribution.xslx')
+        self.assertIn(
+            'Sheet2',
+            workbook.sheetnames
+            'The Workbook should have a Worksheet named "Sheet2"'
+        )
+        self.assertIn(
+            'Sheet1',
+            workbook.sheetnames
+            'The Workbook should have a Worksheet named "Sheet2"'
+        )
 
 
-class TestSheet1(TestCase):
+class TestWorkSheet1(TestCase):
     def test_header_content(self):
         pass
 
-class TestSheet2(TestCase):
+    def test_number_of_rows(self):
+        pass
+
+class TestWorkSheet2(TestCase):
     def test_header_content(self):
+        pass
+
+    def test_number_of_rows(self):
         pass
